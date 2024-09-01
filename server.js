28c17810a299c4693142be1b0fa1236152bcc763
saichakra-dev <gundasaichakra316@gmail.com>
2024-09-01 14:28:41 +0530

    third-commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const expressStatic = require("express").static;
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 // middleware
 app.use(express.json());
